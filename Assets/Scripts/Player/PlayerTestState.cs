ca210b6da666f28c004c85a169b8f99a6df6513a
Miguel Sanchez <mangelsr25@gmail.com>
2025-11-19 21:34:50 -0500

    [ADD] Input interaction and wire with state machine

--- Assets/Scripts/Player/PlayerTestState.cs
+++ Assets/Scripts/Player/PlayerTestState.cs
@@ -2,29 +2,30 @@ using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
-    private float timer = 5f;
+    private float timer;
 
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
     {
         Debug.Log("Enter");
+        stateMachine.InputReader.JumpEvent += OnJump;
     }
 
     public override void Tick(float deltaTime)
     {
-        timer -= deltaTime;
-        Debug.Log("Tick");
-        // Debug.Log($"Remaining time: {timer}");
-
-        if (timer <= 0)
-        {
-            stateMachine.SwitchState(new PlayerTestState(stateMachine));
-        }
+        timer += deltaTime;
+        Debug.Log($"Remaining time: {timer}");
     }
 
     public override void Exit()
     {
         Debug.Log("Exit");
+        stateMachine.InputReader.JumpEvent -= OnJump;
+    }
+
+    private void OnJump()
+    {
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
     }
 }
