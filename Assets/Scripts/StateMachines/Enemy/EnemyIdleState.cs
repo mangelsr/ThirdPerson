21e0a0031798521d28ce6321fcc0a52811b9e7d3
Miguel Sanchez <mangelsr25@gmail.com>
2025-11-26 00:27:18 -0500

    [ADD] Enemy Idle State

---
+++ Assets/Scripts/StateMachines/Enemy/EnemyIdleState.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class EnemyIdleState : EnemyBaseState
+{
+    private readonly int LocomotionBlendTreeHash = Animator.StringToHash("LocomotionBlendTree");
+    private readonly int SpeedHash = Animator.StringToHash("Speed");
+    private const float CrossFadeDuration = 0.1f;
+    private const float AnimatorDampTime = 0.1f;
+
+
+    public EnemyIdleState(EnemyStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        stateMachine.Animator.CrossFadeInFixedTime(LocomotionBlendTreeHash, CrossFadeDuration);
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        stateMachine.Animator.SetFloat(SpeedHash, 0f, AnimatorDampTime, deltaTime);
+    }
+
+    public override void Exit() { }
+}
