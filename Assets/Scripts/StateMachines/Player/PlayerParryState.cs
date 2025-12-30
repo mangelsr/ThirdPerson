8f25cf511b414c63f2cc4d61550b9b21e8c7c543
Miguel Sanchez <mangelsr25@gmail.com>
2025-12-29 23:14:45 -0500

    [UPD] Parry mechanic to apply on the middle of the animation and update ParryWindowTime value reduce to half a second

--- Assets/Scripts/StateMachines/Player/PlayerParryState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerParryState.cs
@@ -5,6 +5,7 @@ public class PlayerParryState : PlayerBaseState
     private readonly int ParryAnimationHash = Animator.StringToHash("Parry");
     private const float CrossFadeDuration = 0.1f;
     private readonly GameObject attacker;
+    private bool parryApplied;
 
     public PlayerParryState(PlayerStateMachine stateMachine, GameObject attacker) : base(stateMachine)
     {
@@ -14,7 +15,6 @@ public class PlayerParryState : PlayerBaseState
     public override void Enter()
     {
         stateMachine.Animator.CrossFadeInFixedTime(ParryAnimationHash, CrossFadeDuration);
-        Parry();    
     }
 
     public override void Tick(float deltaTime)
@@ -22,6 +22,9 @@ public class PlayerParryState : PlayerBaseState
         Move(deltaTime);
 
         float normalizedTime = GetNormalizedTime(stateMachine.Animator, "Parry");
+
+        if (normalizedTime > 0.5f && !parryApplied)
+            Parry();
 
         if (normalizedTime >= 1f)
         {
@@ -57,6 +60,7 @@ public class PlayerParryState : PlayerBaseState
             if (attacker.TryGetComponent(out EnemyStateMachine enemyStateMachine)) {
                 enemyStateMachine.SwitchState(new EnemyImpactState(enemyStateMachine));
             }
+            parryApplied = true;
         }
     }
 }
